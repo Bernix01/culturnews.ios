b481cac21ed19435e7f41160f868f63af18c8380
Guillermo Enrique Bernal Moreira <gbernal096@gmail.com>
2015-06-23 17:10:27 -0500

    Initial Commit

---
+++ CulturNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CulturNews
+//
+//  Created by GUILLERMO BERNAL on 6/23/15.
+//  Copyright (c) 2015 CulturNews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
